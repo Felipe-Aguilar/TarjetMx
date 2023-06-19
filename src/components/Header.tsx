0cb94712b39cb23280f98bd306874809a0a163d4
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2023-06-19 11:21:28 -0600

    Agregando animación de menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import Logo from '../assets/logotarjet.svg';
 import LogoMobile from '../assets/logotarjet-cabezal-tablet.svg';
@@ -64,19 +65,38 @@ const Header = () => {
             </header>
             
             { button &&
-                <div className='bodyMenuMobile'>
+                <AnimatePresence>
+                    { button &&
+                        <motion.div 
+                            className='bodyMenuMobile'
+                            initial={{opacity: 0, scale:0}}
+                            animate={{opacity: 1, scale: 1}}
+                            exit={{scale:0, opacity:0}}
+                        >
+                            <div className='links'>
+                                <a href="">Tienda</a>
+                                <a href="">Empresas</a>
+                                <a href="">Premium</a>
+                                <a href="">Tarjeta Digital</a>
+                                <a href="">Tarjeta física</a>
+                                <a href="">Iniciar sesión</a>
+                                <a href="">Directorio Tarjet</a>
+                                <a href="">Contacto</a>
+                            </div>
 
-                    <div className='links'>
-                        <a href="">Tienda</a>
-                        <a href="">Empresas</a>
-                        <a href="">Premium</a>
-                        <a href="">Tarjeta Digital</a>
-                        <a href="">Tarjeta física</a>
-                        <a href="">Iniciar sesión</a>
-                        <a href="">Directorio Tarjet</a>
-                        <a href="">Contacto</a>
-                    </div>
-                </div>
+                            <hr className='mt-5'/>
+
+                            <div className='social'>
+                                <a href="" target='_blank'>
+                                    <i className="bi bi-facebook"></i>
+                                </a>
+                                <a href="" target='_blank'>
+                                    <i className="bi bi-instagram"></i>
+                                </a>
+                            </div>
+                        </motion.div>
+                    }
+                </AnimatePresence>
             }
         </>
 
