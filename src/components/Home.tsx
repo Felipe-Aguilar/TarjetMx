bbb1b55f5450751deac8f17ff185c18ec71cd6a5
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2023-06-23 15:44:02 -0600

    Todo finalizado menos graficos azules

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -39,6 +39,16 @@ import IconPersoAzul from '../assets/icono-tarjet-tarjetapersonalizableazul.svg'
 import GraficoA from '../assets/grafico-continua-azul-a.svg';
 import GraficoB from '../assets/grafico-continua-azul-b.svg';
 import GraficoC from '../assets/grafico-continua-azul-c.svg';
+import IconPaso1 from '../assets/icono-tarjet-paso1.svg';
+// import IconPaso2 from '../assets/icono-tarjet-paso2.svg';
+// import IconPaso3 from '../assets/icono-tarjet-paso3.svg';
+// import IconPaso4 from '../assets/icono-tarjet-paso4.svg';
+import IconTDCont from '../assets/icono-tarjet-contactotarjetTD.svg';
+import IconTDBolsa from '../assets/icono-tarjet-productoTD.svg';
+import IconTDImagen from '../assets/icono-tarjet-agregarimagenTD.svg';
+import IconTDBusca from '../assets/icono-tarjet-buscadortarjetTD.svg';
+import muestraTarjetero from '../assets/im-tarjet-muestratarjetero-c.webp';
+import Promo from '../assets/Promo.webp';
 
 const Home = () => {
 
@@ -116,6 +126,41 @@ const Home = () => {
         a: GraficoA,
         b: GraficoB,
         c: GraficoC
+    }
+
+    // Animación Icons Tarjetero Digital
+    const iconsTDig = {
+        initial: {scale: 1},
+        whileInView: {scale: [1,1.2,1]},
+        transition: {duration: 1},
+        viewport: {once:true}
+    }
+
+    // Settings Slide Tarjeta Digital Mobile
+    const settingsTFMobile = {
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplaySpeed: 6000,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
     }
 
     return ( 
@@ -1192,9 +1237,32 @@ const Home = () => {
                         <div className='graficos'>
                             <div 
                                 className='grafico'
-                                // style={{background: `url(${graficos.a})`}}
+                                style={{background: `url(${graficos.a})`}}
                             >
-                                <p>Paso1</p>
+                                <div className='body'>
+                                    <img src={IconPaso1} alt="Icon Tarjet"/>
+                                    <p>
+                                        Paso 1
+                                    </p>
+                                    <p className='description'>
+                                        Selecciona la cantidad de tarjetas físicas Tarjet que necesitas.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div 
+                                className='grafico graficoB'
+                                style={{background: `url(${graficos.b})`}}
+                            >
+                                <div className='body bodyB'>
+                                    <img src={IconPaso1} alt="Icon Tarjet"/>
+                                    <p>
+                                        Paso 1
+                                    </p>
+                                    <p className='description'>
+                                        Selecciona la cantidad de tarjetas físicas Tarjet que necesitas.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -1219,6 +1287,250 @@ const Home = () => {
                 </div>
             </div>
 
+            {/* Tarjetero Digital */}
+            <div className='container-fluid TarjeteroDigital tarjeta'>
+                <div className='cuerpo d-none d-lg-block'>
+                    
+                    <div className='block1'>
+                        <div className='w-50'>
+                            <h2>
+                                Tarjetero Digital Tarjet <br/>
+                                <span>
+                                    Lo nuevo de la Red networking de tarjet, ¡utilízalo!
+                                </span>
+                            </h2>
+                            <p className='mt-4'>
+                                Nuestro tarjetero Tarjet es la nueva generación de directorio de tarjetas digitales que facilitan la comunicación entre empresa, la consulta de información y las recomendaciones de mano en mano. 
+                            </p>
+                            <div className='icons'>
+                                <div className='icon'>
+                                    <motion.img 
+                                        src={IconTDCont} alt="Icon Tarjet" 
+                                        {...iconsTDig}
+                                        transition={{delay:0.5}}
+                                    />
+                                    <p>
+                                        Facilidad para consultar tus contactos tarjet guardados.
+                                    </p>
+                                </div>
+                                <div className='icon'>
+                                    <motion.img 
+                                        src={IconTDBolsa} alt="Icon Tarjet" 
+                                        {...iconsTDig}
+                                        transition={{delay:1}}
+                                    />
+                                    <p>
+                                        Encuentra la Información de un servicio, producto o establecimiento de un socio tarjet.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='icons'>
+                                <div className='icon'>
+                                    <motion.img 
+                                        src={IconTDImagen} alt="Icon Tarjet"
+                                        {...iconsTDig}
+                                        transition={{delay:1.5}} 
+                                    />
+                                    <p>
+                                        Genera confianza agregando imágenes a tu galería y llega a más personas.
+                                    </p>
+                                </div>
+                                <div className='icon'>
+                                    <motion.img 
+                                        src={IconTDBusca} alt="Icon Tarjet" 
+                                        {...iconsTDig}
+                                        transition={{delay:2}}
+                                    />
+                                    <p>
+                                        En nuestro buscador puedes ser encontrado fácilmente por personas que buscan lo que haces.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-50 image'>
+                            <img src={muestraTarjetero} alt="Muestra tarjetero tarjet" />
+                        </div>
+                    </div>
+
+                    <div className='block2'>
+                        <div>
+                            <a href="" className='btnMorado'>
+                                Regístrate gratuitamente <br/> y accede a tu tarjetero digital tarjet.
+                            </a>
+                            <p>
+                                No es necesario crear una tarjeta virtual para guardar, recomendar y recibir recomendaciones de socios Tarjet
+                            </p>
+                        </div>
+
+                        <div>
+                            <a href="" className='btnNaranja'>
+                                Ya tienes tu cuenta Tarjet. <br />
+                                Consulta aquí el directorio de socios tarjet.
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className='block3'>
+                        <div className='w-50'>
+                            <img src={Promo} alt="Promocional tarjet" />
+                        </div>
+                        <div className='w-50'>
+                            <h6>
+                                Para todos aquellos que ofrecen un servicio o producto. <br/>
+                            </h6>
+                            <h6 className='mt-4'>
+                                ¡avancemos juntos, registra tu empresa al directorio tarjet!, invíta a la gente a conocer más sobre ti.
+                            </h6>
+                            <p className='mt-5'>
+                                En nuestro directorio de socios tarjet podrás compartir tus datos, imágenes, enlace a tus redes y medios de contacto, de manera fácil, sostenible y de forma gratuita.
+                            </p>
+                            <p>
+                                Quien consulta gratuitamente el directorio de servicios y negocios tarjet, puede acceder a tu información general y guardar los datos en su agenda de contactos tarjet dentro de nuestra plataforma. También pueden recomendarte o compartir tu información en un clic.
+                            </p>
+
+                            <a href="">
+                                Registra gratuitamente tu negocio en el directorio 
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className='block4 text-center mt-3'>
+                        <h5>Hagamos networking y suma más clientes.</h5>
+                    </div>
+
+                </div>
+
+                {/* Mobile */}
+                <div className='cuerpo d-block d-lg-none'>
+                    <h2>
+                        Tarjetero digital Tarjet <br/>
+                        <span>
+                            Lo nuevo de la Red networking, de tarjet, ¡utilízalo!
+                        </span>
+                    </h2>
+                    <p>
+                        Nuestro tarjetero Tarjet es la nueva generación de directorio de tarjetas digitales que facilitan la comunicación entre empresa, la consulta de información y las recomendaciones de mano en mano. 
+                    </p>
+                    <div className='sliderMobile'>
+                        <Slider {...settingsTFMobile}> 
+                            <div className='icon'>
+                                <img 
+                                    src={IconTDCont} alt="Icon Tarjet" 
+                                />
+                                <p>
+                                    Facilidad para consultar tus contactos tarjet guardados.
+                                </p>
+                            </div>
+                            <div className='icon'>
+                                <img 
+                                    src={IconTDBolsa} alt="Icon Tarjet" 
+                                />
+                                <p>
+                                    Encuentra la Información de un servicio, producto o establecimiento de un socio tarjet.
+                                </p>
+                            </div>
+                            <div className='icon'>
+                                <img
+                                    src={IconTDImagen} alt="Icon Tarjet"
+                                />
+                                <p>
+                                    Genera confianza agregando imágenes a tu galería y llega a más personas.
+                                </p>
+                            </div>
+                            <div className='icon'>
+                                <img
+                                    src={IconTDBusca} alt="Icon Tarjet" 
+                                />
+                                <p>
+                                    En nuestro buscador puedes ser encontrado fácilmente por personas que buscan lo que haces.
+                                </p>
+                            </div>
+                        </Slider>
+                    </div>
+                    <div className='banner'>
+                        <img src={muestraTarjetero} alt="Muestra tarjetero tarjet" />
+                    </div>
+                    <div className='block2'>
+                        <a href="" className='btnMorado'>
+                            Regístrate gratuitamente <br/> y accede a tu tarjetero digital tarjet.
+                        </a>
+                        <p className='mt-4 mb-4'>
+                            No es necesario crear una tarjeta virtual para guardar, recomendar y recibir recomendaciones de socios Tarjet
+                        </p>
+                        <a href="" className='btnNaranja'>
+                            Ya tienes tu cuenta Tarjet. <br />
+                            Consulta aquí el directorio de socios tarjet.
+                        </a>
+                    </div>
+                    <div className='banner mt-0'>
+                        <img src={Promo} alt="Muestra tarjetero tarjet" />
+                    </div>
+                    <div className='text'>
+                        <h6>
+                            Para todos aquellos que ofrecen un servicio o producto. <br/>
+                        </h6>
+                        <h6 className='mt-4'>
+                            ¡avancemos juntos, registra tu empresa al directorio tarjet!, invíta a la gente a conocer más sobre ti.
+                        </h6>
+                        <p>
+                            En nuestro directorio de socios tarjet podrás compartir tus datos, imágenes, enlace a tus redes y medios de contacto, de manera fácil, sostenible y de forma gratuita.
+                        </p>
+                        <p>
+                            Quien consulta gratuitamente el directorio de servicios y negocios tarjet, puede acceder a tu información general y guardar los datos en su agenda de contactos tarjet dentro de nuestra plataforma. También pueden recomendarte o compartir tu información en un clic.
+                        </p>
+
+                        <a href="">
+                            Registra gratuitamente tu negocio en el directorio 
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            {/* Btn Contacto */}
+            <div className='container-fluid Contacto'>
+                <div className='d-block d-lg-none'>
+                    <h5>
+                        Hagamos networking y suma más clientes.
+                    </h5>
+                </div>
+                <div className='btnContactanos'>
+                    <a href="">
+                        Contáctanos
+                    </a>
+                </div>
+            </div>
+
+            {/* Preguntas Frecuentes */}
+            <div className='container-fluid PreguntasFrecuentes tarjeta'>
+                <div className='cuerpo'>
+                    <h6>Preguntas frecuentes</h6>
+                    <ul>
+                        <li>¿Qué información puedo poner en mi tarjeta Tarjet?</li>
+                        <li>¿Cómo compro mi tarjeta Tarjet?</li>
+                        <li>¿Qué formas de pago aceptan?</li>
+                        <li>¿El pago es por única vez?</li>
+                        <li>¿Realizan facturas?</li>
+                        <li>¿Tiene vigencia mi tarjeta?</li>
+                        <li>¿Puedo actualizar la información de mi tarjeta Tarjet?</li>
+                        <li>¿Es autoadministrable?</li>
+                        <li>¿Modificar alguna información tiene algún costo extra?</li>
+                        <li>¿Qué tan seguros están mis datos?</li>
+                        <li>¿Cómo guardo mi tarjeta en mi dispositivo?</li>
+                        <li>¿Cómo envío mi tarjeta a mis clientes?</li>
+                        <li>¿Se requiere alguna aplicación de Tarjet.mx?</li>
+                        <li>¿En qué dispositivo se podrá visualizar mi tarjet?</li>
+                        <li>¿Cómo funcionan las tarjetas físicas NFC?</li>
+                        <li>¿Cómo funcionan las tarjetas físicas NFC?</li>
+                        <li>¿En cuánto tiempo recibiré mi tarjeta física NFC?</li>
+                    </ul>
+                    <p>
+                        Nos encantará poder ayudarte a resolver tus dudas. <br/>
+                        <span>
+                            Escríbenos a contacto@tarjet.mx ó llámanos si prefieres hablar con nosotros.
+                        </span>
+                    </p>
+                </div>
+            </div>
         </>
     );
 }
